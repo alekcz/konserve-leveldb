8611d5d499e50375ef5c32704ad8c8a929d24840
Alexander Oloo <alekcz@gmail.com>
2020-09-23 11:44:56 +0200

    update repl

--- project.clj
+++ project.clj
@@ -8,6 +8,6 @@
                  [factual/clj-leveldb "0.1.1"]
                  [byte-streams "0.2.2"] ;required for clj-leveldb to work https://github.com/ztellman/clj-tuple/issues/18
                  [io.replikativ/konserve "0.6.0-alpha1"]]
-  :repl-options {:init-ns konserve-couchdb.core}
+  :repl-options {:init-ns konserve-leveldb.core}
   :plugins [[lein-cloverage "1.2.0"]]
   :profiles { :dev {:dependencies [[metosin/malli "0.0.1-20200404.091302-14"]]}})
